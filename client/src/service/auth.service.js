f2d91dc92125491852cac48d2aa24a54988956ee
Achraf <Achrafarazzouk@gmail.com>
2021-05-11 22:10:52 +0200

    started

---
+++ client/src/service/auth.service.js
@@ -0,0 +1,15 @@
+import axios from 'axios'
+
+class AuthService {
+    constructor() {
+        this.app = axios.create({
+            baseURL: 'http://localhost:5000/api/auth',
+            withCredentials: true
+        })
+    }
+
+    login = userDetails => this.app.post('/login', userDetails)
+    signup = userDetails => this.app.post('/signup', userDetails)
+    logout = () => this.app.get('/logout')
+    isloggedin = () => this.app.post('/isloggedin')
+}
