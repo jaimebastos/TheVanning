35147b43a99d8d890be0477f15d717f3d6b1ef6b
Achraf <achrafarazzouk@gmail.com>
2021-05-12 19:01:42 +0200

    Components done, not ready

--- client/src/components/routes/Routes.js
+++ client/src/components/routes/Routes.js
@@ -3,26 +3,12 @@ import { Switch, Route, Redirect } from 'react-router-dom'
 import Login from '../pages/Login/Login'
 import Signup from '../pages/Signup/Signup'
 
-const Routes = ({ storeUser, loggedUser }) => {
+const Routes = ({ storeUser }) => {
     <Switch>
-        <Route path="/" exact render={ /** Comp index */} />
-        <Route path="/vans" exact render={ /** Comp listadp */} />
-        <Route path="/vans/details/:id" render={ /** Comp list details */} />
-        <Route path="/vans/create" render={ /** Comp vans create */} />
-        <Route path="/vans/update/:id" render={ /** Comp vans update */} />
 
-        Profile
+        <Route path="/auth/signup" render={props => <Signup history={props.history} />} />
+        <Route path="/auth/login" render={props => <Login storeUser={storeUser} history={props.history} />} />
 
-        <Route path="/auth/login" render={ /** Comp vans update */} />
-        <Route path="/auth/signup" render={ /** Comp vans update */} />
-
-        <Route path="/extras/details/:id" render={ /** Comp list details */} />
-        <Route path="/extras/create" render={ /** Comp vans create */} />
-        <Route path="/extras/update/:id" render={ /** Comp vans update */} />
-
-        <Route path="/quotes/details/:id" render={ /** Comp list details */} />
-        <Route path="/quotes/create" render={ /** Comp vans create */} />
-        <Route path="/quotes/update/:id" render={ /** Comp vans update */} />
     </Switch>
 }
 
--- client/src/service/auth.service.js
+++ client/src/service/auth.service.js
@@ -12,4 +12,5 @@ class AuthService {
     signup = userDetails => this.app.post('/signup', userDetails)
     logout = () => this.app.get('/logout')
     isloggedin = () => this.app.post('/isloggedin')
+    isAdmin = () => this.app.post('/isAdmin')
 }
