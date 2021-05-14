4aa2564b6a6f0d771d84451d676aff31491b818f
Guille Muñoz <guillermomunozgonzalez@MacBook-Pro-de-Guillermo.local>
2021-05-14 09:56:23 +0200

    quote in progress

--- client/src/components/pages/Quote/QuoteCard.js
+++ client/src/components/pages/Quote/QuoteCard.js
@@ -1,11 +1,11 @@
 import { Form, Button, Container, Row, Col } from 'react-bootstrap'
 import { Component } from "react";
 
-import QuoteService from "../../../service/Quote.service";
 import DesignerVan from './QuoteDesignerVan';
 import DimensionVan from './QuoteDimensionVan';
 import SpecificationsVan from './QuoteSpecificationsVan';
 import ExtrasService from '../../../service/extras.service';
+
 
 
 class QuoteCard extends Component {
@@ -39,33 +39,36 @@ class QuoteCard extends Component {
         .getAllExtras()
         .then(response =>{
             this.setState({extras: response.data})
-            console.log('los extras', response)
+            console.log('los extras', response.data)
         })
         .catch(err => console.log('error', err))
   }
-  handleOnSubmit(e){
-       e.preventDefault();
-       const obj = this.state;
-      
-
-  }
-
+  
   designerOnChange(designer) {
-      console.log('antess',designer)
-    this.setState({vans:{ designer }})
-    console.log('despues',this.state.vans)
+
+    this.setState({vans: {designer }})
   }
+
+  handleOnSubmit(e){
+
+       e.preventDefault();
+       const obj = this.state.vans
+       console.log("el objeto que se envia", obj);
+  }
+
   dimensionOnChange(dimension) {
          
-    this.setState({vans :{ dimension }});
-
+    this.setState( {vans:{ dimension }} );
   }
+
   specificationsOnChange(specifications) {
          
     this.setState({vans :{ specifications }});
   }
 
   render() {
+    const {extras} = this.state
+    console.log('extrasss', extras)
     return (
       <Container>
         <Form onSubmit={(e) => this.handleSubmit(e)}>
@@ -78,7 +81,7 @@ class QuoteCard extends Component {
 
             <Col md={6}>
               <SpecificationsVan  onInputChange={(e) => this.specificationsOnChange(e)} />
-
+              {}
             </Col>
           </Row>
 
