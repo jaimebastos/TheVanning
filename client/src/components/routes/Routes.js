ce2a2f5c94170a4e967e102b96eeaad78e8a31b5
Achraf <Achrafarazzouk@gmail.com>
2021-05-20 15:40:12 +0200

    Fixed protected routes

--- client/src/components/routes/Routes.js
+++ client/src/components/routes/Routes.js
@@ -20,13 +20,13 @@ const Routes = ({ storeUser, loggedUser }) => {
   return (
     <Switch>
       <Route path="/" exact render={() => <LandingPage />} />
-      <Route path="/vans" exact render={() => <Vans />} />
+      <Route path="/vans" exact render={() => <Vans loggedUser={loggedUser} />} />
       <Route path="/vans/details/:id" render={(props) => <VanDetails {...props} />} />
-      <ProtectedRoute path="/vans/create" condition={loggedUser && loggedUser.role === "admin"} component={(props) => <NewVan history={props.history} />} />
-      <Route path="/vans/edit/:vans_id" render={(props) => <EditVan {...props} history={props.history} />} />
+      <ProtectedRoute path="/vans/create" condition={loggedUser && loggedUser.role === "ADMIN"} component={(props) => <NewVan history={props.history} />} />
+      <ProtectedRoute path="/vans/edit/:vans_id" condition={loggedUser && loggedUser.role === "ADMIN"} component={(props) => <EditVan {...props} history={props.history} />} />
       <Route path="/auth/signup" render={(props) => <Signup history={props.history} />} />
       <Route path="/extras" exact render={() => <Extras />} />
-      <Route path="/extras/create" render={(props) => <NewExtra history={props.history} />} />
+      <ProtectedRoute path="/extras/create" condition={loggedUser && loggedUser.role === "ADMIN"} component={(props) => <NewExtra history={props.history} />} />
       <Route path="/auth/login" render={(props) => <Login storeUser={storeUser} history={props.history} />} />
       <Route path="/quote" exact render={() => <QuotePage />} />
       <ProtectedRoute path="/quote/create" condition={loggedUser} component={<QuoteCard loggedUser={loggedUser} />} />
