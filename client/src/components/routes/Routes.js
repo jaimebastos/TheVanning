76cde2563577789805e93e60e87e5fc636a77817
Guille Muñoz <guillermomunozgonzalez@MacBook-Pro-de-Guillermo.local>
2021-05-21 12:03:49 +0200

    amonoooooos

--- client/src/components/routes/Routes.js
+++ client/src/components/routes/Routes.js
@@ -23,16 +23,36 @@ const Routes = ({ storeUser, loggedUser, handleAlert }) => {
       <Route path="/" exact render={() => <LandingPage />} />
       <Route path="/vans" exact render={() => <Vans loggedUser={loggedUser} />} />
       <Route path="/vans/details/:id" render={(props) => <VanDetails {...props} />} />
-      <ProtectedRoute path="/vans/create" condition={loggedUser && loggedUser.role === "ADMIN"} component={(props) => <NewVan history={props.history} />} />
-      <ProtectedRoute path="/vans/edit/:vans_id" condition={loggedUser && loggedUser.role === "ADMIN"} component={(props) => <EditVan {...props} history={props.history} />} />
+      <ProtectedRoute
+        path="/vans/create"
+        condition={loggedUser && loggedUser.role === "ADMIN"}
+        component={(props) => <NewVan history={props.history} />}
+      />
+      <ProtectedRoute
+        path="/vans/edit/:vans_id"
+        condition={loggedUser && loggedUser.role === "ADMIN"}
+        component={(props) => <EditVan {...props} history={props.history} />}
+      />
       <Route path="/auth/signup" render={(props) => <Signup history={props.history} handleAlert={handleAlert} />} />
-      <Route path="/extras" exact render={() => <Extras />} />
-      <ProtectedRoute path="/extras/create" condition={loggedUser && loggedUser.role === "ADMIN"} component={(props) => <NewExtra history={props.history} handleAlert={handleAlert} />} />
+      <Route path="/extras" exact render={() => <Extras loggedUser={loggedUser} />} />
+      <ProtectedRoute
+        path="/extras/create"
+        condition={loggedUser && loggedUser.role === "ADMIN"}
+        component={(props) => <NewExtra history={props.history} handleAlert={handleAlert} />}
+      />
       <Route path="/auth/login" render={(props) => <Login storeUser={storeUser} history={props.history} handleAlert={handleAlert} />} />
       <Route path="/quote" exact render={() => <QuotePage loggedUser={loggedUser} />} />
-      <ProtectedRoute path="/quote/create" condition={loggedUser} component={(props) => <QuoteCard loggedUser={loggedUser} history={props.history} handleAlert={handleAlert} />} />
+      <ProtectedRoute
+        path="/quote/create"
+        condition={loggedUser}
+        component={(props) => <QuoteCard loggedUser={loggedUser} history={props.history} handleAlert={handleAlert} />}
+      />
       <Route path="/extras/:category" render={(props) => <FilteredExtras {...props} />} />
-      <ProtectedRoute path="/my-profile/:id" condition={loggedUser} component={(props) => <MyProfile loggedUser={loggedUser} handleAlert={handleAlert} {...props} />} />
+      <ProtectedRoute
+        path="/my-profile/:id"
+        condition={loggedUser}
+        component={(props) => <MyProfile loggedUser={loggedUser} handleAlert={handleAlert} {...props} />}
+      />
     </Switch>
   );
 }
